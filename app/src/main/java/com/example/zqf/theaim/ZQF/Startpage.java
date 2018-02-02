9e35e192d43d641216fe48c50865462486f4e3ab
Tick <1758828569@qq.com>
2018-02-02 21:36:13 +0800

    张启帆；工程框架

---
+++ app/src/main/java/com/example/zqf/theaim/ZQF/Startpage.java
@@ -0,0 +1,15 @@
+package com.example.zqf.theaim.ZQF;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.zqf.theaim.R;
+
+public class Startpage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_startpage);
+    }
+}
