b8d0588f14ad3d05b967dcc5db340cab06c73452
ck <1404695921@qq.com>
2018-02-26 11:07:50 +0800

    Ck_Calendar

--- app/src/main/java/com/example/zqf/theaim/Fragment/TodayFragment.java
+++ app/src/main/java/com/example/zqf/theaim/Fragment/TodayFragment.java
@@ -24,6 +24,7 @@ public class TodayFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        //测试
         return inflater.inflate(R.layout.fragment_today, container, false);
     }
 
